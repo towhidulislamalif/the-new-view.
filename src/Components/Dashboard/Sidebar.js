f58e5d014c43a7f5142bee002f5f6a13d29d63ba
towhidulislamalif <towhidulislamalif@gmail.com>
2022-12-06 10:02:32 +0600

    some changes

--- src/Components/Dashboard/Sidebar.js
+++ src/Components/Dashboard/Sidebar.js
@@ -1,35 +1,38 @@
-import React, { useContext, useState } from 'react'
-import { ArrowRightOnRectangleIcon, Bars3Icon } from '@heroicons/react/24/solid'
-import { Link } from 'react-router-dom'
-import PrimaryButton from '../Button/PrimaryButton'
-import { AuthContext } from '../../contexts/AuthProvider'
-import UserMenu from './UserMenu'
-import AdminMenu from './AdminMenu'
-import HostMenu from './HostMenu'
+import React, { useContext, useState } from 'react';
+import {
+  ArrowRightOnRectangleIcon,
+  Bars3Icon,
+} from '@heroicons/react/24/solid';
+import { Link } from 'react-router-dom';
+import PrimaryButton from '../Button/PrimaryButton';
+import { AuthContext } from '../../contexts/AuthProvider';
+import UserMenu from './UserMenu';
+import AdminMenu from './AdminMenu';
+import HostMenu from './HostMenu';
 
 const Sidebar = ({ role, loading }) => {
-  const { user, logout } = useContext(AuthContext)
-  const [isActive, setActive] = useState('false')
+  const { user, logout } = useContext(AuthContext);
+  const [isActive, setActive] = useState('false');
 
   // Sidebar Responsive Handler
   const handleToggle = () => {
-    setActive(!isActive)
-  }
+    setActive(!isActive);
+  };
   return (
     <>
       {/* Small Screen Navbar */}
-      <div className='bg-gray-100 text-gray-800 flex justify-between md:hidden'>
+      <div className="bg-gray-100 text-gray-800 flex justify-between md:hidden">
         <div>
-          <div className='block cursor-pointer p-4 font-bold'>
-            <Link to='/'>AirCnC</Link>
+          <div className="block cursor-pointer p-4 font-bold">
+            <Link to="/">The New View</Link>
           </div>
         </div>
 
         <button
           onClick={handleToggle}
-          className='mobile-menu-button p-4 focus:outline-none focus:bg-gray-700'
+          className="mobile-menu-button p-4 focus:outline-none focus:bg-gray-700"
         >
-          <Bars3Icon className='h-5 w-5' />
+          <Bars3Icon className="h-5 w-5" />
         </button>
       </div>
       {/* Sidebar */}
@@ -41,25 +44,25 @@ const Sidebar = ({ role, loading }) => {
         <div>
           {/* Branding & Profile Info */}
           <div>
-            <h2 className='text-3xl cursor-pointer font-semibold text-center text-gray-800 '>
-              <Link to='/'> AirCnC</Link>
+            <h2 className="text-3xl cursor-pointer font-semibold text-center text-gray-800 ">
+              <Link to="/"> The New View</Link>
             </h2>
-            <div className='flex flex-col items-center mt-6 -mx-2'>
-              <Link to='/dashboard'>
+            <div className="flex flex-col items-center mt-6 -mx-2">
+              <Link to="/dashboard">
                 <img
-                  className='object-cover w-24 h-24 mx-2 rounded-full'
+                  className="object-cover w-24 h-24 mx-2 rounded-full"
                   src={user?.photoURL}
-                  alt='avatar'
-                  referrerPolicy='no-referrer'
+                  alt="avatar"
+                  referrerPolicy="no-referrer"
                 />
               </Link>
-              <Link to='/dashboard'>
-                <h4 className='mx-2 mt-2 font-medium text-gray-800  hover:underline'>
+              <Link to="/dashboard">
+                <h4 className="mx-2 mt-2 font-medium text-gray-800  hover:underline">
                   {user?.displayName}
                 </h4>
               </Link>
-              <Link to='/dashboard'>
-                <p className='mx-2 mt-1 text-sm font-medium text-gray-600  hover:underline'>
+              <Link to="/dashboard">
+                <p className="mx-2 mt-1 text-sm font-medium text-gray-600  hover:underline">
                   {user?.email}
                 </p>
               </Link>
@@ -67,7 +70,7 @@ const Sidebar = ({ role, loading }) => {
           </div>
 
           {/* Nav Items */}
-          <div className='flex flex-col justify-between flex-1 mt-6'>
+          <div className="flex flex-col justify-between flex-1 mt-6">
             <nav>
               {role && role !== 'requested' ? (
                 <>{role === 'admin' ? <AdminMenu /> : <HostMenu />} </>
@@ -82,16 +85,16 @@ const Sidebar = ({ role, loading }) => {
           <hr />
           <PrimaryButton
             handler={logout}
-            classes='flex block w-full rounded-full items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform'
+            classes="flex block w-full rounded-full items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform"
           >
-            <ArrowRightOnRectangleIcon className='w-5 h-5' />
+            <ArrowRightOnRectangleIcon className="w-5 h-5" />
 
-            <span className='mx-4 font-medium'>Logout</span>
+            <span className="mx-4 font-medium">Logout</span>
           </PrimaryButton>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
